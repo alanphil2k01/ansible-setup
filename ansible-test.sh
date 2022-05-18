c58ce442c732ad7656184bf69e114c4c72b59eb0
alanphil2k01 <alanphilipose@gmail.com>
2022-05-19 01:18:46 +0530

    fix ssh

--- ansible-test.sh
+++ ansible-test.sh
@@ -2,4 +2,4 @@
 
 docker build -t test-ansible .
 
-docker run --rm -it -t test-ansible ansible-playbook --skip-tags dconf local.yml --ask-become-pass --ask-vault-pass  && bash
+docker run --rm -it -t test-ansible sh -c "ansible-pull -K --vault-password-file .vault -U https://github.com/alanphil2k01/ansible-setup.git || bash"
