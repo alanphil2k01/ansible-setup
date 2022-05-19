0670e30275ef9d93dba015a3f5d8cf7549ac4ced
alanphil2k01 <alanphilipose@gmail.com>
2022-05-19 08:21:20 +0530

    add zsh and fix tmux install

--- ansible-test.sh
+++ ansible-test.sh
@@ -2,4 +2,4 @@
 
 docker build -t test-ansible .
 
-docker run --rm -it -t test-ansible sh -c "ansible-pull -K --vault-password-file .vault -U https://github.com/alanphil2k01/ansible-setup.git || bash"
+docker run --rm -it -t test-ansible bash -c "ansible-pull --skip-tags dconf -K --vault-password-file .vault -U https://github.com/alanphil2k01/ansible-setup.git || bash"
